7a8e0983ef05ce9003ee6f8c78b675805639018c
Stephen Benner <Benners727@gmail.com>
2023-08-22 19:57:45 -0400

    Figured out how to add custom villager trade.

---
+++ src/main/java/net/steve/vanillaoverhaulmod/event/ModEvents.java
@@ -0,0 +1,31 @@
+package net.steve.vanillaoverhaulmod.event;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraftforge.event.village.VillagerTradesEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.steve.vanillaoverhaulmod.VanillaOverhaulMod;
+
+import java.util.List;
+
+
+@Mod.EventBusSubscriber(modid = VanillaOverhaulMod.MOD_ID)
+public class ModEvents {
+    @SubscribeEvent
+    public static void addCustomTrades(VillagerTradesEvent event) {
+        if(event.getType() == VillagerProfession.LIBRARIAN) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(Items.ACACIA_DOOR, 1);
+            int villagerLevel = 1;
+
+            trades.get(villagerLevel).clear();
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 2),
+                    stack,10,8,0.02F));
+        }
+    }
+}
